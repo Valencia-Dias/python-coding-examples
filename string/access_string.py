abcffe81764ddc7c39d459476d48a30ac0755320
Valencia Dias <dias.val.jose@gmail.com>
2024-01-30 16:50:52 +0530

    String operations

--- string/access_string.py
+++ string/access_string.py
@@ -8,4 +8,5 @@ print(s[-2:])
 print(s[-4:])
 print(s[-4:-2])
 print(s[-4:-1])
+print(s[6:-2])
 
---
+++ string/count_char_sequence.py
@@ -0,0 +1,8 @@
+s ="This is a bad bad bad idea this is really very very weird"
+print(s.count("is"))
+print(s.count("bad"))
+print(s.count("This"))
+s1 = s.upper()
+print(s1.count("IS"))
+print(s1.count("BAD"))
+print(s1.count("THIS"))
